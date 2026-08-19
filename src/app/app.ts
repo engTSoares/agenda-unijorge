f3929ae123bf3878ce2c483576c530e59dc4dba5
Tiago Soares <engtiagosoares44@gmail.com>
2026-08-19 13:46:45 -0300

    labs02

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { disciplinas } from './labs/labs02';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/labs/labs02.ts
@@ -0,0 +1,5 @@
+export const disciplinas = [
+  { nome: 'Desenv. Dinâmico', ch: 80, nota: 8.5 },
+  { nome: 'Banco de Dados',   ch: 80, nota: 6.0 },
+  { nome: 'Engenharia de SW', ch: 40, nota: 9.1 },
+];
