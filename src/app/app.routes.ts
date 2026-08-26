4fb6a244ed2c5b94dbe1fb4fb43469a1be79392f
Tiago Soares <engtiagosoares44@gmail.com>
2026-08-26 14:02:24 -0300

    aula02-exerc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Pessoa } from './components/pessoa/pessoa'; 
+import { Home } from './components/home/home';
+import { NaoEncontrada } from './components/nao-encontrada/nao-encontrada';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: 'home',
+    pathMatch: 'full' },
+    { path: '',   component: Home    },
+    {
+        path: '', component: Pessoa
+    }, 
+    { path: '**', component: NaoEncontrada }
+];
--- src/app/components/pessoa/pessoa.ts
+++ src/app/components/pessoa/pessoa.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './pessoa.html',
   styleUrl: './pessoa.css',
 })
-export class Pessoa {}
+export class Pessoa {
+  nome = 'Ana Souza';
+  curso = 'Engenharia de Computação';
+  email= 'anasouza@email.com'
+}
